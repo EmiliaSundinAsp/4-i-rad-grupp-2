e855329167a009856e2548e727986e3d9eec4607
Emily Wahlin <emilysaletti@gmail.com>
2024-09-06 11:33:25 +0200

    installed react-router-dom and added route to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
+import LandingPage from './views/landingPage';
+// import PlayerVsPlayerComponent from './views/PlayerVsPlayerComponent'
 
+
+
+const App: React.FC = () => {
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path='/player-vs-player' element={<PlayerVsPlayerComponent />} />
+        <Route path="/computer-vs-player" element={<ComputerVsPlayer />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ src/views/landingPage.tsx
@@ -0,0 +1,5 @@
+const landingPage: React.FC = () => {
+  return <h1>Welcome to  Connect four</h1>;
+};
+
+export default landingPage;
