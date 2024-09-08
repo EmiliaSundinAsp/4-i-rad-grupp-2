526c05ed952e2d888c9eea2fd949020f120053d7
Emily Wahlin <emilysaletti@gmail.com>
2024-09-08 23:15:17 +0200

    started writing the constructor for game.ts

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,4 +1,35 @@
+import { useState, useEffect } from 'react'
+
+import Board from './Board.js'
+import MoveHandler from './MoveHandler.js'
+import Player from './Player.js'
+import WinChecker from './WinChecker.js'
+
+
 export default class Game {
-  
+
+  board: Board;
+  playerX!: Player;
+  playerO!: Player;
+  moveHandler: MoveHandler;
+  winChecker: WinChecker;
+  currentPlayer!: Player;
+  gameOver: boolean;
+
+  constructor() {
+    this.board = new Board();
+
+    this.gameOver = false
+    this.createPlayer();
+
+    while (true) {
+
+      this.board = new Board();
+      this.winChecker = new WinChecker(this.board);
+      this.moveHandler = new MoveHandler(this.board, [this.playerX, this.playerO], this);
+
+
+  }
+
 
 }
