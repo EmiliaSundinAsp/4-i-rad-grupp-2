94ac5a8430e1b00c023c960e5814c2b8b01bfe50
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-09-09 09:03:39 +0200

    Add return to run the function RenderBoard

--- src/classes/Board.tsx
+++ src/classes/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Board = () => {
   // Creates and initialize the game board using state
@@ -6,35 +6,31 @@ const Board = () => {
     Array.from({ length: 6 }, () =>
       Array.from({ length: 7 }, () => ' '))
   );
-}
 
-// Function to render the board as JSX
-const renderBoard = () => {
+  // Function to render the board as JSX
+  const renderBoard = () => {
+    return (
+      <div>
+        {Board.map((row, rowIndex) => (
+          <div key={rowIndex}>
+            {row.map((column, colIndex) => (
+              <span key={colIndex}>
+                {`| ${column}`}
+              </span>
+            ))}
+            <span>|</span>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div>
-      {Board.map((row, rowIndex) => (
-        <div key={rowIndex}>
-          {row.map((column, colIndex) => (
-            <span key={colIndex}>
-              {`| ${column}`}
-            </span>
-          ))}
-          <span>|</span>
-        </div>
-      ))}
+      <h1>Game Board</h1>
+      {renderBoard()}
     </div>
-  );
-}
-/*
-  // Function to render the game board in the console
-render() {
-  const line = '\n' + '-'.repeat(29) + '\n';
-  console.log(
-    line +
-    this.gameBoard.map(row =>
-      row.map(column => `| ${column} `).join('') + '|').join(line) + line
-  );
-}
-  */
+  )
+};
 
 export default Board;
